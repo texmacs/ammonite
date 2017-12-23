40e4efb28ecc06c1b1a8fe6408ebf6f1ff59345a
Darcy Shen <sadhen@zoho.com>
2017-12-24 01:11:28 +0800

    dummy scala launcher

---
+++ progs/init-scala.scm
@@ -0,0 +1,47 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : init-python.scm
+;; DESCRIPTION : Initialize python plugin
+;; COPYRIGHT   : (C) 2004  Ero Carrera,
+;;               (C) 2012  Adrian Soto
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-modules (dynamic session-edit) (dynamic program-edit))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Plugin configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;Basically, the serializer makes the input preserve the newlines
+;;and adds the string character "\n<EOF>\n" by the end.
+;;I guess it could send "\x04" instead to signal a real EOF,
+;;but I would need to check if that does not kill the pipe...
+;;An alternative approach is to use the input-done? command
+;;from TeXmacs, but, at the time of this writing, it did not work.--A
+
+(define (python-serialize lan t)
+  (with u (pre-serialize lan t)
+    (with s (texmacs->code (stree->tree u))
+      (string-append  s  "\n<EOF>\n"))))
+
+(define (scala-launcher)
+  (if (os-mingw?)
+      "tm_python.bat --texmacs"
+      "tm_python3 --texmacs"))
+
+(plugin-configure python
+  (:require (url-exists-in-path? "python"))
+  (:require (url-exists-in-path? "tm_python3"))
+  (:launch ,(python-launcher))
+  (:tab-completion #t)
+  (:serializer ,python-serialize)
+  (:session "Scala")
+  (:scripts "Scala"))
+
+;(set-session-multiline-input "python" "default" #t)
+;(set-program-multiline-input "python" "default" #t)
